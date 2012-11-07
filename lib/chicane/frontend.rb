acd586c25ba5f252ea2c9230ea9018ff31a6df4e
JD Huntington <jdhuntington@gmail.com>
2012-11-06 21:11:11 -0800

    Serve index.html from '/'

--- lib/chicane/frontend.rb
+++ lib/chicane/frontend.rb
@@ -3,5 +3,9 @@ require 'sinatra'
 module Chicane
   class Frontend < Sinatra::Base
     set :public_folder, File.expand_path('../../../static', __FILE__)
+
+    get '/' do
+      send_file File.join(settings.public_folder, 'index.html')
+    end
   end
 end
